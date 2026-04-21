b7fd437658cab248aa764e462dee12cfe69e4f40
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-04-21 15:22:18 +0300

    task-2 done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,42 @@
 'use strict';
-console.log('test');
+
+const form = document.querySelector('.feedback-form');
+const fillKey = 'feedback-form-state';
+
+const formData = {
+    email: '',
+    message: '',
+};
+
+const savedData = localStorage.getItem(fillKey);
+if (savedData) {
+    const parsedData = JSON.parse(savedData);
+
+    formData.email = parsedData.email ?? '';
+    formData.message = parsedData.message ?? '';
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', (event) => {
+    const { name, value } = event.target;
+    formData[name] = value.trim();
+    localStorage.setItem(fillKey, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (formData.email === '' || formData.message === '') {
+        alert('Fill please all fields');
+        return;
+    }
+    console.log(formData);
+
+    localStorage.removeItem(fillKey);
+
+    formData.email = '';
+    formData.message = '';
+
+    form.reset();
+});
